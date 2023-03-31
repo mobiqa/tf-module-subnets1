c4383db90e55fd23ffa74acae80d6dd3b27d6ee8
venkat <venkat.mobiqa@gmail.com>
2023-03-31 18:43:48 +0530

    updating ./tf-module-subnets1

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "main" {
   count = length(var.cidr_block)
   cidr_block = var.cidr_block[count.index]
-  availability_zone = var.availability_zones[count.index]
+  availability_zone = var.availability_zone[count.index]
   vpc_id = var.vpc_id
 
   tags = merge(
